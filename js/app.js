165d1ddbb2892b0ab5a13b7e68a63aa40a86751a
ArannaBarua1971 <arannabaruase71@gmail.com>
2022-09-04 23:05:40 +0600

    new things and new file added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider').slick({
+        prevArrow:"<i class='icon fas fa-chevron-left'></i>",
+        nextArrow:'<i class="icon fas fa-chevron-right"></i>',
+        autoplay:true,
+        speed:700,
+        fade:true,
+        cssEase: 'linear'
+    });
+})
