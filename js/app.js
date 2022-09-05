10f8e0c3df7c89f79f0fd06b3df0a0d28bc16ccf
ArannaBarua1971 <arannabaruase71@gmail.com>
2022-09-06 01:37:03 +0600

    new things added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 $(function(){
+
+  // for peloader
+  $(window).on('load',function(){
+    $("#preloader").fadeOut(500)
+  })
+
+  // slick slider part in banner
     $('.slider').slick({
         prevArrow:"<i class='icon fas fa-chevron-left'></i>",
         nextArrow:'<i class="icon fas fa-chevron-right"></i>',
@@ -7,4 +14,33 @@ $(function(){
         fade:true,
         cssEase: 'linear'
     });
+
+    // typed.js use for banner
+    var typed = new Typed('.change01', {
+        strings: ['Trendy Design',
+        '          Show OFF Design', 
+                  'Futureistic Electronic Item'],
+        smartBackspace: false,
+        loop:true,
+        typeSpeed:70,
+        backSpeed:70,
+      });
+    var typed = new Typed('.change02', {
+        strings: ['Trendy Design',
+        '          Show OFF Design', 
+                  'Futureistic Electronic Item'],
+        smartBackspace: false,
+        loop:true,
+        typeSpeed:70,
+        backSpeed:70,
+      });
+    var typed = new Typed('.change03', {
+        strings: ['Trendy Design',
+        '          Show OFF Design', 
+                  'Futureistic Electronic Item'],
+        smartBackspace: false,
+        loop:true,
+        typeSpeed:70,
+        backSpeed:70,
+      });
 })
